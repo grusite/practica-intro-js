fb4dae9c16b222d3c6b7ba969560eeae86299bd2
Jorge <grusite@gmail.com>
2019-07-11 02:31:46 +0200

    working on Poker Kata

--- Poker/index.mjs
+++ Poker/index.mjs
@@ -1,3 +1,41 @@
-import { Deck } from "./utils.mjs";
+import {
+  Deck,
+  Card,
+  handRanked,
+  isSameColor,
+  hasConsecutiveValues
+} from "./utils.mjs";
 
-console.log(new Deck().getRandomHand());
+const result = { win: 1, loss: 2, tie: 3 };
+const ranks = [
+  "Straight flush",
+  "Four of a kind",
+  "Full house",
+  "Flush",
+  "Straight",
+  "Three of a kind",
+  "Two pairs",
+  "Pair",
+  "High card"
+];
+
+const card = new Card("A", "H");
+console.log(card);
+
+// const hand = new Hand();
+// console.log("Hand " + hand.getRandomHand());
+
+const deck1 = new Deck();
+const player1Hand = deck1.getRandomHand();
+const player2Hand = deck1.getRandomHand();
+
+const myHand = ["2D", "3D", "4D", "5D", "6D"];
+const player3Hand = deck1.setCustomHand(myHand);
+
+console.log("Cartas del jugador 3: " + player3Hand.map(card => card.getCard()));
+console.log("Cartas color del j3: " + isSameColor(player3Hand));
+console.log("Valores cons del j3: " + hasConsecutiveValues(player3Hand));
+console.log("Cartas Ranked del j1: " + handRanked(player1Hand));
+console.log("Cartas del jugador 1: " + player1Hand.map(card => card.getCard()));
+console.log("Cartas del jugador 2: " + player2Hand.map(card => card.getCard()));
+console.log("Resultado: Jugador 1 gana, carta más alta");
--- Poker/utils.mjs
+++ Poker/utils.mjs
@@ -1,7 +1,15 @@
+// "H" -> "Hearts" red, "S" -> "Spades" picas black, "C" -> "Clubs" treboles black, "D" -> "Diamonds" red
+const suits = ["H", "S", "C", "D"];
+const red = ["H", "D"];
+const black = ["S", "C"];
+// "A" -> "Ace", "J" -> "Jack", "Q" -> "Queen", "K" -> "King"
+const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
 class Card {
   constructor(value, suit) {
     this.value = value;
     this.suit = suit;
+    this.rank = values.indexOf(this.value);
   }
   getCard() {
     return this.value + this.suit;
@@ -12,17 +20,29 @@ class Deck {
   constructor() {
     this.deck = [];
 
-    // "H" -> "Hearts", "S" -> "Spades", "C" -> "Clubs", "D" -> "Diamonds"
-    const suits = ["H", "S", "C", "D"];
-    // "A" -> "Ace", "J" -> "Jack", "Q" -> "Queen", "K" -> "King"
-    const values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-
     for (let suit in suits) {
       for (let value in values) {
         const card = new Card(values[value], suits[suit]);
-        this.deck.push(card.getCard());
+        this.deck.push(card);
       }
     }
+  }
+
+  getDeck() {
+    for (let i = 0; i < this.deck.length; i++) {
+      this.deck[i] = this.deck[i].getCard();
+    }
+    return this.deck;
+  }
+
+  setCustomHand(hand) {
+    this.hand = [];
+
+    for (let i = 0; i < hand.length; i++) {
+      hand[i] = new Card(hand[i].substr(0, 1), hand[i].substr(1, 1));
+      this.hand.push(hand[i]);
+    }
+    return this.hand;
   }
 
   getRandomHand() {
@@ -39,4 +59,51 @@ class Deck {
   }
 }
 
-export { Card, Deck };
+// class Hand {
+//   constructor() {
+//     this.hand = [];
+//   }
+
+//   setCustomHand(hand) {
+//     for (let i = 0; i < hand.length; i++) {
+//       hand[i] = new Card(hand[i].substr(0, 1), hand[i].substr(1, 1));
+//       this.hand.push(hand[i]);
+//     }
+//     return this.hand;
+//   }
+
+//   getRandomHand() {
+//     let randomDeckNumber = 0;
+//     const handCards = 5;
+//     const deck = new Deck();
+
+//     for (let i = 0; i < handCards; i++) {
+//       randomDeckNumber = Math.floor(Math.random() * this.deck.length);
+//       this.hand.push(deck.deck[randomDeckNumber]);
+//     }
+//     return this.hand;
+//   }
+// }
+
+const handRanked = hand => hand.map(card => card.rank);
+
+const isSameColor = hand =>
+  hand.map(card => card.suit).every((val, i, arr) => val === arr[0]);
+
+const hasConsecutiveValues = function(hand) {
+  return hand
+    .map(card => parseInt(card.value))
+    .sort()
+    .every((val, i, arr) => {
+      if (i == arr.length - 1) return true;
+      return val === arr[i + 1] - 1;
+    });
+};
+
+const repeatedValues = function(hand) {
+  hand.map(card => parseInt(card.value)).sort();
+};
+
+const straightFlush = function(hand) {};
+
+export { Card, Deck, handRanked, isSameColor, hasConsecutiveValues };
